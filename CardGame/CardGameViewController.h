0dc1f1c88ad9131eff5b5be77167d68564f8fb62
Laura Brown <laurabrown1113@gmail.com>
2014-01-13 15:38:09 -0500

    Initial Commit

---
+++ CardGame/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  CardGame
+//
+//  Created by artsy on 1/13/14.
+//  Copyright (c) 2014 Laura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ CardGame/CardGameViewController.m
@@ -0,0 +1,29 @@
+//
+//  CardGameViewController.m
+//  CardGame
+//
+//  Created by artsy on 1/13/14.
+//  Copyright (c) 2014 Laura. All rights reserved.
+//
+
+#import "CardGameViewController.h"
+
+@interface CardGameViewController ()
+
+@end
+
+@implementation CardGameViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
