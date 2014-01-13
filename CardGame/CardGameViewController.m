51c0798882ba06ea72bb99d7b40977a908321edf
Laura Brown <laurabrown1113@gmail.com>
2014-01-13 17:51:33 -0500

    flipping the card over

--- CardGame/CardGameViewController.m
+++ CardGame/CardGameViewController.m
@@ -26,4 +26,17 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)touchCardButton:(UIButton *)sender {
+    if ([sender.currentTitle length]){
+        
+        [sender setBackgroundImage:[UIImage imageNamed:@"card_back"]
+                                              forState:UIControlStateNormal];
+        [sender setTitle:@"" forState:UIControlStateNormal];
+    } else {
+        [sender setBackgroundImage:[UIImage imageNamed:@"card_front"]
+                          forState:UIControlStateNormal];
+        [sender setTitle:@"A♣︎" forState:UIControlStateNormal];
+    }
+}
+
 @end
